d0fffd2730b9f8b0c539eb822eb78e3d33d2ea8b
nipunnyka <nipunika.co18@nsut.ac.in>
2020-07-24 20:45:57 +0530

    added new Round

---
+++ CodeForces/Round-659/2C.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include<bits/stdc++.h>
+#define fi first
+#define se second
+#define pi pair<int, int>
+#define pb push_back
+#define mp make_pair
+#define ptr (*it)
+#define ll long long
+#include <chrono> 
+#define _USE_MATH_DEFINES
+#define vl vector<ll>
+#define vi vector<int>
+#include <cmath>
+#include <iostream>
+#define pli pair<ll, int>
+#define pl pair<ll, ll>
+
+#define FAST ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+using namespace std;
