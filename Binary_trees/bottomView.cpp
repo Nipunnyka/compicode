ee9dafb1377db322dca2422a8d9d0627a9da04e0
Nipunika <50680604+Nipunnyka@users.noreply.github.com>
2021-07-31 21:32:16 +0530

    Create bottomView.cpp

---
+++ Binary_trees/bottomView.cpp
@@ -0,0 +1,74 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <bits/stdc++.h>
+
+#define mp make_pair
+#define fi first
+#define se second
+#define pb push_back
+
+using namespace std;
+
+struct node{
+    int key;
+    node* left;
+    node* right;
+    node(int key) {
+        this->key = key;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void bottomView(node* root) {
+    if(root == NULL) 
+        return;
+    
+    unordered_map<int, node*> distanceMap;
+    queue<pair<node*, int>> q;
+    
+    q.push(mp(root, 0));
+    
+    while(!q.empty()) {
+        pair<node*, int> t = q.front();
+        q.pop();
+        
+        distanceMap[t.se] = t.fi;
+        
+        if(t.fi->left)
+            q.push(mp(t.fi->left, t.se-1));
+            
+        if(t.fi->right)
+            q.push(mp(t.fi->right, t.se+1));
+        
+    }
+    
+    for(auto x: distanceMap) {
+        cout<<x.se->key<<" ";
+    }
+
+}
+
+int main()
+{
+    struct node* root = new node(20); 
+	root->left = new node(8); 
+	root->left->left = new node(4); 
+	root->left->right = new node(12); 
+	root->left->right->left = new node(10); 
+	root->left->right->right = new node(14); 
+	root->right = new node(22); 
+	root->right->right = new node(25); 
+	
+	int height =0;
+	
+	bottomView(root);
+	
+    return 0;
+}
